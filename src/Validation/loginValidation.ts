b216d93806780ac749382d720a5e37acb518c16b
damfer01 <kauanrenata2@gmail.com>
2024-04-12 14:52:56 -0300

    foi tudo

--- src/Validation/loginValidation.ts
+++ src/Validation/loginValidation.ts
@@ -4,23 +4,17 @@ import {
 
 
 export const loginValidation = [
-  //loginValidator:()=> {
-
-  //validação  do CNPJ
+  
   body('cnpj').isNumeric().isLength({
     min: 14, max: 14
   }).withMessage('CNPJ  obrigatorio ter 14 digitos'),
 
   body('cnpj').escape().not().isEmpty().withMessage('CNPJ  obrigatorio'),
 
-
-  //Validação  do Email
-  //body('email').escape().not().isEmpty().isEmail().withMessage('Email invalido'),
-
   body('empresa').escape().not().isEmpty().withMessage('Empresa  obrigatorio'),
 
-  
   body('password').escape().not().isEmpty().withMessage('senha  obrigatorio'),
+
   body('password').isLength({
     min: 8, max: 20
   }).withMessage(' no minimo 8 digitos'),
